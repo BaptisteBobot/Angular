aaa743c024d5682867646567b963c599a08d4e65
BaptisteBobot <b.bobot@webetsolutions.com>
2019-05-24 10:47:01 +0200

    signup

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular';
+  constructor(){
+    const firebaseConfig = {
+      apiKey: "AIzaSyAFpiOLnFDa9RaO6ycM2PK8Fc87n3Qm4nM",
+      authDomain: "angularend.firebaseapp.com",
+      databaseURL: "https://angularend.firebaseio.com",
+      projectId: "angularend",
+      storageBucket: "angularend.appspot.com",
+      messagingSenderId: "514898815650",
+      appId: "1:514898815650:web:3ce106078702ed79"
+    };
+    firebase.initializeApp(firebaseConfig);
+  }
 }
